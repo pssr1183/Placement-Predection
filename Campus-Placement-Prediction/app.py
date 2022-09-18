524106bb2fbcf2f4008a96e6858dc5138344648f
pssr1183 <76833054+pssr1183@users.noreply.github.com>
2022-09-18 20:45:05 +0530

    Add files via upload

---
+++ Campus-Placement-Prediction/app.py
@@ -0,0 +1,105 @@
+import numpy as np
+import re
+from flask import Flask, request, render_template, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import pickle
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
+# Create flask app
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+app.secret_key = 'your secret key'
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'poorna1183'
+app.config['MYSQL_DB'] = 'mylogin'
+ 
+mysql = MySQL(app)
+ 
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM flasks_db WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+ 
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+ 
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM flasks_db WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO flasks_db VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
+
+# prediction function
+def ValuePredictor(to_predict_list):
+	to_predict = np.array(to_predict_list).reshape(1, 12)
+	loaded_model = pickle.load(open("model.pkl", "rb"))
+	result = loaded_model.predict(to_predict)
+	return result[0]
+
+@app.route("/")
+def Login():
+    return render_template("login.html")
+@app.route("/index.html")
+def Home():
+    print('Request for index page received')
+    return render_template("index.html")
+
+@app.route("/result", methods = ["POST"])
+def result():
+    print('Request for predict page received')
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        if int(result)== 1:
+            prediction ='Placed'
+        else:
+            prediction ='Not Placed'
+    return render_template("result.html", prediction_text = prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
